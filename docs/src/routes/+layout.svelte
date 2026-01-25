8a0ee9e512e458070ed58803914152dd97891150
eytienne <arabeyre.etienne@gmail.com>
2026-01-25 18:35:23 +0100

    docs

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link href="//fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" type="text/css">
+</svelte:head>
+{@render children()}
