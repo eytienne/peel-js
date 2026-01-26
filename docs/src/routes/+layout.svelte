5c383877a84815a2f74b3b5b6873031317eca174
eytienne <arabeyre.etienne@gmail.com>
2026-01-26 17:42:58 +0100

    fix seo

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 </script>
 
 <svelte:head>
+	<title>Peel.js documentaion</title>
+	<meta name="description" content="Javascript micro library for creating peeling effects using just HTML5">
 	<link rel="icon" href={favicon} />
 	<link href="//fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" type="text/css">
 </svelte:head>
